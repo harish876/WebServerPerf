857bd785d0d3c9754f143f3b3ec47f02c3fdf6d0
harish876 <harishgokul01@gmail.com>
2025-02-27 23:13:39 +0000

    add new performance result files and refactor CMake project naming

--- http-server-rust-vanilla/src/connection_handler.rs
+++ http-server-rust-vanilla/src/connection_handler.rs
@@ -1,5 +1,6 @@
 use std::io::{BufReader, prelude::*};
 use std::net::TcpStream;
+use std::os::fd::{FromRawFd, RawFd};
 
 pub trait HandleConnection {
     fn handle_connection(self);
@@ -36,5 +37,44 @@ impl HandleConnection for TcpStream {
             response = "HTTP/1.1 200 OK\r\n\r\n".to_string();
         }
         self.write_all(response.as_bytes()).unwrap();
+    }
+}
+
+impl HandleConnection for RawFd {
+    fn handle_connection(self) {
+        let mut tcp_stream: TcpStream = unsafe { TcpStream::from_raw_fd(self) };
+        if let Err(e) = tcp_stream.set_nonblocking(true) {
+            eprintln!("Error in turning FD into non blocking stream {}", e);
+        }
+
+        let mut buf_reader = BufReader::new(&tcp_stream);
+        let mut buffer = String::new();
+
+        // Read the request line
+        if buf_reader.read_line(&mut buffer).is_err() {
+            eprintln!("Failed to read line");
+            return;
+        }
+
+        // Parse the request line
+        let mut parts = buffer.split_whitespace();
+        let method = parts.next().unwrap_or("");
+        let path = parts.next().unwrap_or("");
+
+        //println!("Method: {}, Path: {}", method, path);
+
+        let mut response = String::from("HTTP/1.1 404 NotFound\r\n\r\n");
+        if method == "GET" && path.contains("/echo/") {
+            let content = &path[6..];
+            let formatted_response = format!(
+                "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                content.len(),
+                content
+            );
+            response = formatted_response;
+        } else if method == "GET" && path == "/" {
+            response = "HTTP/1.1 200 OK\r\n\r\n".to_string();
+        }
+        tcp_stream.write_all(response.as_bytes()).unwrap();
     }
 }
